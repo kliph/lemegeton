f62d068e03ddeec1a918c49b6b37d128ad88f200
Cliff Rodgers <kliph@users.noreply.github.com>
2017-09-05 19:42:24 -0400

    Add tests for loading and parsing json

--- project.clj
+++ project.clj
@@ -4,10 +4,13 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
                  [org.clojure/clojurescript "1.9.908"]
-                 [com.cemerick/piggieback "0.2.2"]]
+                 [com.cemerick/piggieback "0.2.2"]
+                 [lein-doo "0.1.7"]]
   :repl-options {:init-ns dev.repl}
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :plugins [[lein-cljsbuild "1.1.6"]]
+  :clean-targets ^{:protect false} [:target-path "out" "main.js" "test.js"]
+  :plugins [[lein-cljsbuild "1.1.6"]
+            [lein-doo "0.1.7"]]
   :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.13"]]
                    :source-paths ["src" "dev"]
                    :cljsbuild {:builds [{:id "dev"
@@ -18,4 +21,10 @@
                                                     :output-dir "out"
                                                     :target :nodejs
                                                     :npm-deps {:emojilib "2.2.9"}
-                                                    :install-deps false}}]}}})
+                                                    :install-deps false}}
+                                        {:id "test"
+                                         :source-paths ["src" "test"]
+                                         :compiler {:output-to "test.js"
+                                                    :output-dir "out"
+                                                    :target :nodejs
+                                                    :main "lemegeton.runner"}}]}}})
