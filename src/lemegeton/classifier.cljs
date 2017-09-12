ea00cfbd069486889872aba6c0656e1b1c80acf0
Cliff Rodgers <kliph@users.noreply.github.com>
2017-09-11 21:15:26 -0400

    Takes 30s to churn through small data setalpha
    - Going to need to preprocess and store computation in file

--- src/lemegeton/classifier.cljs
+++ src/lemegeton/classifier.cljs
@@ -17,12 +17,47 @@
       clojure.string/lower-case
       word-set))
 
-(defn almost-all-keys []
+(defn all-keys []
   (let [ks (keys data/emojis)
-        kws (reduce (fn [acc emoji]
-                      (if (:keywords emoji)
-                        (apply conj acc (:keywords emoji))
-                        acc))
-                    #{}
-                    (vals data/emojis))]
+        kws (-> (reduce (fn [acc emoji]
+                          (if (:keywords emoji)
+                            (apply conj acc (:keywords emoji))
+                            acc))
+                        #{}
+                        (vals data/emojis))
+                (into (map name (keys data/emojis))))]
     kws))
+
+(defn levenshtein-distance [s t]
+  (let [f (fn [f s t]
+            (cond
+              (empty? s) (count t)
+              (empty? t) (count s)
+              :else (let [cost (if (= (first s) (first t))
+                                 0
+                                 1)]
+                      (min (inc (f f (rest s) t))
+                           (inc (f f s (rest t)))
+                           (+ cost (f f (rest s) (rest t)))))))
+        g (memoize f)]
+    (g g s t)))
+
+(defn my-main []
+  (let [words (classify)
+        all-keys (all-keys)
+        distances (fn [word] (reduce (fn [acc k]
+                                       (let [d (levenshtein-distance word k)
+                                             prev-distance (-> acc
+                                                               vals
+                                                               first)]
+                                         (if (zero? prev-distance)
+                                           (reduced acc)
+                                           (if (< prev-distance d)
+                                             acc
+                                             (do
+                                               (js/console.log k d acc)
+                                               (assoc {} k d))))))
+                                     {:invalid 1000}
+                                     (shuffle all-keys)))
+        foo (map distances words)]
+    foo))
