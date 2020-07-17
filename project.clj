50c3dbcc03504b80c2a8b1b9daa57a80b1c9e656
Cliff Rodgers <kliph@users.noreply.github.com>
2020-07-17 07:58:50 -0400

    Update CircleCI config
    - Required upgrading deps to fix outdated
      java.lang.ClassNotFoundException: javax.xml.bind.DatatypeConverter

--- project.clj
+++ project.clj
@@ -2,16 +2,16 @@
   :description ""
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
-                 [org.clojure/clojurescript "1.9.908"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.773"]
                  [com.cemerick/piggieback "0.2.2"]
-                 [lein-doo "0.1.7"]]
+                 [lein-doo "0.1.11"]]
   :repl-options {:init-ns dev.repl}
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :clean-targets ^{:protect false} [:target-path "out" "main.js" "test.js"]
   :plugins [[lein-cljsbuild "1.1.6"]
             [lein-doo "0.1.7"]]
-  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.13"]]
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.20"]]
                    :source-paths ["src" "dev"]
                    :cljsbuild {:builds [{:id "dev"
                                          :source-paths ["src"]
